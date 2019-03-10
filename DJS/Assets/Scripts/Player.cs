15a474440c870ff592bc1adea2f8ecab3db77d8d
kimgaejin <kimgaejin@naver.com>
2019-03-10 19:53:35 +0900

    간단 이동만 구현
    - 아직 기획안이 제대로 나오질 않아서...
    - 진~ 짜 좌우이동만 구현

---
+++ DJS/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+    }
+}
