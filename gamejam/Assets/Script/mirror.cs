adeb3d573b06a8cd5ff620c16b6252e1c733a842
agw5256 <agw5256@naver.com>
2019-05-12 17:54:48 +0900

    거울 클릭시 90도 회전 추가

---
+++ gamejam/Assets/Script/mirror.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirror : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        transform.Rotate(new Vector3(0, 0, 90));
+        if (this.gameObject.tag == "mirror12") {
+            this.gameObject.tag = "mirror41";
+        }
+       else if (this.gameObject.tag == "mirror41")
+        {
+            this.gameObject.tag = "mirror34";
+        }
+       else if (this.gameObject.tag == "mirror34") {
+            this.gameObject.tag = "mirror23";
+        }
+        else if(this.gameObject.tag == "mirror23") {
+            this.gameObject.tag = "mirror12";
+        }
+    }
+}
