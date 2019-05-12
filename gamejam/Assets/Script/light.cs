4f2be473787d5598bf6df0721071fd0b96f4f970
agw5256 <agw5256@naver.com>
2019-05-12 17:10:27 +0900

    거울 4개버젼 추가

--- gamejam/Assets/Script/light.cs
+++ gamejam/Assets/Script/light.cs
@@ -37,19 +37,76 @@ public class light : MonoBehaviour
             if (start == 0)
             {
                 transform.position += Vector3.right * speed * Time.deltaTime;
+                wasd = 2;
             }
             else if (start == 1 ) {
                 transform.position += Vector3.left * speed * Time.deltaTime;
+                wasd = 4;
             }
         }
         else if (wasd == 1) {
             transform.position += Vector3.up * speed * Time.deltaTime;
         }
+        else if (wasd == 2)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else if (wasd == 3)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        else if (wasd == 4)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
     }
     void OnTriggerEnter2D(Collider2D col) {
-        if (col.gameObject.tag.Equals("mirrorup"))
+        // 거울 위 오른
+        if (col.gameObject.tag.Equals("mirror12"))
         {
-            wasd = 1;
+            if (wasd == 3)
+            {
+                wasd = 2;
+            }
+            else if (wasd == 4) {
+                wasd = 1;
+            }
+        }
+        //거울 오른 아래
+        if (col.gameObject.tag.Equals("mirror23"))
+        {
+            if (wasd == 4)
+            {
+                wasd = 3;
+            }
+            else if (wasd == 1)
+            {
+                wasd = 2;
+            }
+        }
+        //거울 아래 왼
+        if (col.gameObject.tag.Equals("mirror34"))
+        {
+            if (wasd == 1)
+            {
+                wasd = 4;
+            }
+            else if (wasd == 2)
+            {
+                wasd = 3;
+            }
+        }
+        //거울 왼 위
+        if (col.gameObject.tag.Equals("mirror41"))
+        {
+            if (wasd == 2)
+            {
+                wasd = 1;
+            }
+            else if (wasd == 3)
+            {
+                wasd = 4;
+            }
         }
         /////포탈 충돌관련 함수
         for (int i = 0; i < 10; i++)
