b7495e589aa22f457a9537d4aba0d6ee222c784d
agw5256 <agw5256@naver.com>
2019-07-17 21:46:19 +0900

    사라지는 발판
    사라질 시간 생성 시간 추가

---
+++ gamejam/Assets/Script/dieblock.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dieblock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject block;
+    public int destroytime;
+    public int regeneratetime;
+    private int fDestroyTime = 2;
+    private int fTickTime;
+    private int fTickTime1;
+    private int c_enter;
+    private Vector2 pos;
+    void Start()
+    {
+        destroytime = 100;
+        regeneratetime = 300;
+        fTickTime = 0;
+        fTickTime1 = 0;
+        c_enter = 0;
+        pos = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(fTickTime + "   " + fTickTime1);
+        fTickTime = fTickTime + 1;
+        if (fTickTime - fTickTime1 >= destroytime && c_enter == 1)
+        {
+           // Debug.Log("asd");
+            Block_setActive_false();
+        }
+        if (fTickTime - fTickTime1 >= regeneratetime && fTickTime - fTickTime1 <= regeneratetime+30) {
+            //Debug.Log("asd");
+            Block_setActive_true();
+            c_enter = 0;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            c_enter = 1;
+          //  Debug.Log(fTickTime);
+            fTickTime1 = fTickTime;
+            
+        }
+
+    }
+    void Block_setActive_true() {
+
+        transform.position = pos;
+        // block.gameObject.SetActive(true);
+    }
+    void Block_setActive_false()
+    {
+        transform.position = new Vector2(10000, 100000);
+        //  block.gameObject.SetActive(false);
+    }
+
+}
