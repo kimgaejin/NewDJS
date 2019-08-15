3e2923f69784dc4b935f7fd80de8773009d6e395
kimgaejin <kimgaejin@naver.com>
2019-08-15 22:53:27 +0900

    선풍기와 레버벨트 추가
    +선풍기는 온오프 기능이 없는 CollerOnly만 추가
    + 레버로 키고 끌 수 있는 벨트 추가

---
+++ gamejam/Assets/Script/Lever_Belt.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever_Belt : MonoBehaviour
+{
+    //===========================
+    // 기획자를 위한 주석
+    [Header("Belts의 하위에 컨베이어벨트들을 복사해서 쓰시면 됩니다.")]
+
+    //===========================
+    // 수정 가능한 값
+    [Space(5)]
+    [Header("모든 컨베이어 스피드 * 0이면 벨트 각자의 속도에 따릅니다.")]
+    public float speed = 0f;
+
+    [Header("레버가 오른쪽 일 때 실행")]
+    [Tooltip("false라면 레버가 왼쪽으로 갔을 때 실행됩니다.")]
+    public bool operateWhenLeverIsRight = true;
+
+    //===========================
+    // 임의로 바꾸지 마시오
+    private Animator anim;
+
+    private MovingWorkRight[] belts;
+
+    // player의 transform.position으로 판독.
+    private bool isPlayerLeft;
+    private bool isPlayerRight;
+    private bool isPlayerLeftBefore;
+    private bool isPlayerRightBefore;
+    private bool isLeverLeft = true;
+    private bool isLeverRight = false;
+
+    private int beltSize;
+
+    //===========================
+    // 함수부
+
+    private void Awake()
+    {
+        Transform beltParent = transform.GetChild(1);
+        {   // 컨베이어벨트 초기화
+            beltSize = beltParent.childCount;
+            belts = new MovingWorkRight[beltSize];
+
+            for (int i = 0; i < beltSize; i++)
+            {
+                belts[i] = beltParent.GetChild(i).GetComponent<MovingWorkRight>();
+            }
+            Debug.Log("size: " + beltSize);
+        }
+
+        {   // 애니메이션 초기화
+            Transform graphic = transform.GetChild(0);
+            Debug.Log("graphics: " + graphic.name);
+            anim = graphic.GetComponent<Animator>();
+            
+        }
+    }
+
+    private void Start()
+    {
+        AllBeltStop();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ExecuteWithLight(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x < this.transform.position.x)
+            {
+                isPlayerLeft = true;
+                isPlayerRight = false;
+            }
+            else
+            {
+                isPlayerLeft = false;
+                isPlayerRight = true;
+            }
+        }
+
+        if (isPlayerLeftBefore && isPlayerRight)
+        {   // 레버가 오른쪽으로 돌아갔다.
+            LeverTurnRight();
+
+            if (operateWhenLeverIsRight) AllBeltExecute();
+            if (!operateWhenLeverIsRight) AllBeltStop();
+        }
+
+        if (isPlayerRightBefore && isPlayerLeft)
+        {   // 레버가 왼쪽으로 돌아갔다.
+            LeverTurnLeft();
+
+            if (operateWhenLeverIsRight) AllBeltStop();
+            if (!operateWhenLeverIsRight) AllBeltExecute();
+        }
+
+        isPlayerLeftBefore = isPlayerLeft;
+        isPlayerRightBefore = isPlayerRight;
+    }
+
+    private void ExecuteWithLight(Collider2D collision)
+    {
+        // 빛하고 닿았을 때, 실행되는 방향으로 레버를 돌린다.
+        if (collision.tag == "light")
+        {
+            if (operateWhenLeverIsRight)
+            {
+                LeverTurnRight();
+            }
+            else
+            {
+                LeverTurnLeft();
+            }
+        }
+
+    }
+
+    private void LeverTurnLeft()
+    {
+        isLeverLeft = true;
+        isLeverRight = false;
+        anim.SetTrigger("handleToLeft");
+    }
+
+    private void LeverTurnRight()
+    {
+        isLeverLeft = false;
+        isLeverRight = true;
+        anim.SetTrigger("handleToRight");
+    }
+
+    private void AllBeltExecute()
+    {
+        for (int i = 0; i < beltSize; i++)
+        {
+            if (belts[i])
+            {
+                belts[i].isExecute = true;
+                if (speed != 0)
+                    belts[i].speed = speed;
+            }
+        }
+    }
+
+    private void AllBeltStop()
+    {
+        for (int i = 0; i < beltSize; i++)
+        {
+            if (belts[i])
+            {
+                belts[i].isExecute = false;
+               // Debug.Log("쓰답");
+            }
+        }
+    }
+}
--- gamejam/Assets/Script/MovingWorkRight.cs
+++ gamejam/Assets/Script/MovingWorkRight.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class MovingWorkRight : MonoBehaviour
 {
-    /* 오른쪽으로만 움직이는 컨베이어 벨트
-     * 
-     * 논란:
-     * - 무빙워크가 두 개 이상 겹쳐있을 때, 다음 무빙워크에 막혀서 전진이 안되던데. 그것을 고치기 위해 AddForce로 위로 띄움.
-     * - 근데 그렇게 하니 되게 부들부들대면서 진행함. 나중에 문제가 있으면 수정하고 아니면 그냥 사용.
-     * 
-     * - transform.position을 직접 옮겨서 바꾸니 해결은 되지만, 이제 경사로가 안됨.
-     */
-
     public float speed = 1;
 
-    //private Vector3 risingPower = new Vector3(0, Time.deltaTime, 0);
+    public bool isExecute = true;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "platform") return;
-        //if (collision.tag == "danger") return;
+        if (isExecute == false)
+        {
+            ReturnGravity(collision);
+            return;
+        }
 
         Rigidbody2D rigid;
         rigid = collision.GetComponent<Rigidbody2D>();
@@ -34,20 +29,20 @@ public class MovingWorkRight : MonoBehaviour
         Vector3 power = new Vector3(xRatio * speed, yRatio * Mathf.Abs(speed), 0);
         collision.transform.position += power / rigid.mass * Time.deltaTime;
 
-        //rigid.AddForce(-Physics2D.gravity, ForceMode2D.Impulse);
         rigid.gravityScale = 0;
-        //rigid.velocity = Vector2.zero;
-        //rigid.AddForce(power , ForceMode2D.Impulse);
-
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "platform") return;
-        //if (collision.tag == "danger") return;
 
+        ReturnGravity(collision);
+    }
+
+    private void ReturnGravity(Collider2D collision)
+    {
+        // 0으로 만든 중력을 원래대로 되돌립니다. 기본값 2
         Rigidbody2D rigid;
-
         rigid = collision.GetComponent<Rigidbody2D>();
         if (rigid)
         {
