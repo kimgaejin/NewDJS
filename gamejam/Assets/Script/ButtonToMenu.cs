e55e998250a19018b1694444e598202078238f4d
agw5256 <agw5256@naver.com>
2019-09-07 15:40:25 +0900

    ui적용

--- gamejam/Assets/Script/ButtonToMenu.cs
+++ gamejam/Assets/Script/ButtonToMenu.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ButtonToMenu : MonoBehaviour
 {
+    public GameObject StopPanel;
     public void GotoMain()
     {
+        //SceneManager.LoadScene("UI");
+        StopPanel.SetActive(true);
+    }
+    public void pExit()
+    {
+        //SceneManager.LoadScene("UI");
+        StopPanel.SetActive(false);
+    }
+    public void Menubutton()
+    {
         SceneManager.LoadScene("UI");
+       
     }
 }
