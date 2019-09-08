dbf265c8f3f2ef187f89b0115d5a4be684b6e3ce
agw5256 <agw5256@naver.com>
2019-09-08 20:41:39 +0900

    초단위변경및 외각부분 충돌제외

--- gamejam/Assets/Script/dieblock.cs
+++ gamejam/Assets/Script/dieblock.cs
@@ -6,19 +6,30 @@ public class dieblock : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject block;
-    public int destroytime;
-    public int regeneratetime;
+    //public int destroytime;
+   // public int regeneratetime;
+    /*
     private int fDestroyTime = 2;
     private int fTickTime;
     private int fTickTime1;
+    */
+    private float Step_time;
+    public float destroytime;
+    public float regeneratetime;
+
+
     private int c_enter;
     private Vector2 pos;
     void Start()
     {
-        destroytime = 100;
-        regeneratetime = 300;
+
+        destroytime = 2;
+        regeneratetime = 3;
+        /*
         fTickTime = 0;
         fTickTime1 = 0;
+        */
+
         c_enter = 0;
         pos = this.gameObject.transform.position;
     }
@@ -26,6 +37,7 @@ public class dieblock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         //Debug.Log(fTickTime + "   " + fTickTime1);
         fTickTime = fTickTime + 1;
         if (fTickTime - fTickTime1 >= destroytime && c_enter == 1)
@@ -38,14 +50,26 @@ public class dieblock : MonoBehaviour
             Block_setActive_true();
             c_enter = 0;
         }
+        */
+        if (c_enter == 1 && (Time.time - Step_time) > destroytime) {
+            Block_setActive_false();
+        }
+        if (c_enter == 1 && (Time.time - Step_time) > (regeneratetime + destroytime)) {
+
+            Block_setActive_true();
+            c_enter = 0;
+        }
     }
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag.Equals("Player"))
         {
             c_enter = 1;
+            /*
           //  Debug.Log(fTickTime);
             fTickTime1 = fTickTime;
+            */
+            Step_time = Time.time;
             
         }
 
