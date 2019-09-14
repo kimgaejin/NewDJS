4a7e0987289944b4443ec8a48b04b5a6e08d8e20
kimgaejin <kimgaejin@naver.com>
2019-09-14 16:08:57 +0900

    과학챕터 승강기 추가
    +움직이는 플랫폼으로 승강기같은 느낌
    +플랫폼을 포탈시키는 프리팹 추가

---
+++ gamejam/Assets/Script/DevelopmentDebugingKey.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevelopmentDebugingKey : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O)) Time.timeScale += 0.1f;
+        if (Input.GetKeyDown(KeyCode.P)) Time.timeScale -= 0.1f;
+
+    }
+}
---
+++ gamejam/Assets/Script/Portal_IO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal_IO : MonoBehaviour
+{
+    // 현재는 플랫폼과 닿았을 때 그 플랫폼을 이동시키는 역할
+    private Vector3 outputPos;
+
+    private void Awake()
+    {
+        outputPos = transform.GetChild(0).transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "platform")
+        {
+            collision.transform.position = outputPos;
+        }
+    }
+}
