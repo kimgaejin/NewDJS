eddb688d9d28157af4529c39fe9ab54c51db6caf
kimgaejin <kimgaejin@naver.com>
2019-09-14 22:06:19 +0900

    과학챕터 추가중
    +세로 승강기 마저 채우고 끝내면 될듯...

--- gamejam/Assets/Script/DevelopmentDebugingKey.cs
+++ gamejam/Assets/Script/DevelopmentDebugingKey.cs
@@ -6,8 +6,11 @@ public class DevelopmentDebugingKey : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O)) Time.timeScale += 0.1f;
-        if (Input.GetKeyDown(KeyCode.P)) Time.timeScale -= 0.1f;
-
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(KeyCode.O)) Time.timeScale += 0.1f;
+            if (Input.GetKeyDown(KeyCode.P)) Time.timeScale -= 0.1f;
+            Debug.Log("TimeScale: " + Time.timeScale);
+        }
     }
 }
