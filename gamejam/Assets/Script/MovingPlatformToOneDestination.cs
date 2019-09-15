1e6431a3ae78ac0d31fbdf99fc8a5cc0499c8996
kimgaejin <kimgaejin@naver.com>
2019-09-15 16:17:07 +0900

    과학 챕터 최적화
    +승강기를 프리팹으로 따로 만들고 연산 최소화
    +한 층의 승강기는 하나의 오브젝트. 충돌처리 최소화.
    +로직 자체도 MovingPlatformToOneDestination 스크립트에서는 MovingPlatform과 달리 굉장히 적은 연산을 거침

---
+++ gamejam/Assets/Script/MovingPlatformToOneDestination.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformToOneDestination : MonoBehaviour
+{
+    // 하나의 목적지로만 계속 가려는 플랫폼입니다.
+    // 중간에 플랫폼포탈같은걸 이용해서 목적지로 못가게 해주면 됩니다.
+    // 그냥 플랫폼포탈 + 부유플랫폼 만으로는 최적화같은게 귀찮아서 만들었습니다.
+    // 부유 플랫폼보다 호출 함수가 적습니다.
+
+    [Header("*주의* 이거 MovingPlatform 아님.")]
+    [Header("무조건 point[0]으로만 갑니다. 나머지는 신경도 안씁니다.")]
+
+    public float speed = 1.0f;
+
+    [Space(5)]
+    [Tooltip("게임을 시작 했을 때, points들이 투명이 됩니다.")]
+    public bool isPointsTransparentInGame = true;
+
+    private GameObject target;
+    private Transform pointParent;
+    private Vector3[] points;
+    private int pointSize;
+
+    private void Awake()
+    {
+        target = transform.GetChild(0).gameObject;
+        pointParent = transform.GetChild(1);
+
+        pointSize = pointParent.childCount;
+        points = new Vector3[pointSize];
+
+        for (int i = 0; i < pointSize; i++)
+        {
+            points[i] = pointParent.GetChild(i).position;
+            if (isPointsTransparentInGame)
+            {
+                SpriteRenderer pSpr = pointParent.GetChild(i).GetComponent<SpriteRenderer>();
+                if (pSpr) pSpr.color = new Color(0, 0, 0, 0);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 distance = points[0] - target.transform.position;
+        Vector3 deltaSpeed = distance.normalized * speed * Time.deltaTime;
+
+        target.transform.position += deltaSpeed;
+    }
+}
