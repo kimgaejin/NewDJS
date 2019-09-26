0cb44957b15659ade7698e824eaa00c5e720bfae
kimgaejin <kimgaejin@naver.com>
2019-09-26 21:45:19 +0900

    터치시 검은 연출
    . 왼쪽, 오른쪽 이동, 점프 버튼을 누르면 누르고 있는 동안 검어지도록 적용

---
+++ gamejam/Assets/Script/BlackOnTouch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BlackOnTouch : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private Image img;
+    private Color touchedColor;
+    private Color originColor;
+
+    private void Awake()
+    {
+        img = GetComponent<Image>();
+        if (img)
+        {
+            originColor = img.color;
+            touchedColor = img.color;
+            touchedColor.r = touchedColor.r / 2;
+            touchedColor.g = touchedColor.g / 2;
+            touchedColor.b = touchedColor.b / 2;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData pointer)
+    {
+        img.color = touchedColor;
+    }
+
+    public void OnPointerUp(PointerEventData pointer)
+    {
+        img.color = originColor;
+    }
+
+}
