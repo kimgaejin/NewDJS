47dd79132b0df2f9b512084334e948d55d15feda
agw5256 <agw5256@naver.com>
2019-09-26 22:22:50 +0900

    level 5추가

--- gamejam/Assets/Script/UIM.cs
+++ gamejam/Assets/Script/UIM.cs
@@ -8,10 +8,12 @@ public class UIM : MonoBehaviour
     public GameObject[] level2 = new GameObject[5];
     public GameObject[] level3 = new GameObject[5];
     public GameObject[] level4  = new GameObject[5];
+    public GameObject[] level5 = new GameObject[5];
     public int stage1;
     public int stage2;
     public int stage3;
     public int stage4;
+    public int stage5;
     // Start is called before the first frame update
     void Start()
     {    
@@ -19,6 +21,7 @@ public class UIM : MonoBehaviour
         stage2 = PlayerPrefs.GetInt("StageB2"); //게임 시작시 현재까지 깬 스테이지 로드
         stage3 = PlayerPrefs.GetInt("StageB3"); //게임 시작시 현재까지 깬 스테이지 로드
         stage4 = PlayerPrefs.GetInt("StageB4"); //게임 시작시 현재까지 깬 스테이지 로드
+        stage5 = PlayerPrefs.GetInt("StageB5"); //게임 시작시 현재까지 깬 스테이지 로드
 
         if (stage1 == 0)
         {
@@ -49,8 +52,15 @@ public class UIM : MonoBehaviour
             PlayerPrefs.SetInt("Stage4", 31); // 플레그 접촉시 스테이지 저장
             PlayerPrefs.SetInt("StageB4", 31); // 플레그 접촉시 스테이지 저장
         }
+        if (stage5 == 0)
+        {
+            stage5 = 41;
 
-        Debug.Log("현재 스테이지" + stage1 + "   " + stage2 + "   " + stage3 + "   " + stage4);
+            PlayerPrefs.SetInt("Stage5", 41); // 플레그 접촉시 스테이지 저장
+            PlayerPrefs.SetInt("StageB5", 41); // 플레그 접촉시 스테이지 저장
+        }
+
+        Debug.Log("현재 스테이지" + stage1 + "   " + stage2 + "   " + stage3 + "   " + stage4+ "    " +stage5);
         for (int i = 0; i < 5; i++) {
             if (stage1 - int.Parse(level1[i].tag) >= 0)
             {
@@ -97,6 +107,17 @@ public class UIM : MonoBehaviour
                 level4[i].SetActive(false);
             }
         }
+        for (int i = 0; i < 5; i++)
+        {
+            if (stage5 - int.Parse(level5[i].tag) >= 0)
+            {
+                level5[i].SetActive(true);
+            }
+            else
+            {
+                level5[i].SetActive(false);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -106,6 +127,7 @@ public class UIM : MonoBehaviour
         stage2 = PlayerPrefs.GetInt("StageB2"); //게임 시작시 현재까지 깬 스테이지 로드
         stage3 = PlayerPrefs.GetInt("StageB3"); //게임 시작시 현재까지 깬 스테이지 로드
         stage4 = PlayerPrefs.GetInt("StageB4"); //게임 시작시 현재까지 깬 스테이지 로드
+        stage5 = PlayerPrefs.GetInt("StageB5"); //게임 시작시 현재까지 깬 스테이지 로드
         for (int i = 0; i < 5; i++)
         {
             if (stage1 - int.Parse(level1[i].tag) >= 0)
@@ -154,6 +176,18 @@ public class UIM : MonoBehaviour
                 level4[i].SetActive(false);
             }
         }
+        for (int i = 0; i < 5; i++)
+        {
+            if (stage5 - int.Parse(level5[i].tag) >= 0)
+            {
+                level5[i].SetActive(true);
+            }
+            else
+            {
+                level5[i].SetActive(false);
+            }
+        }
+
 
     }
 }
