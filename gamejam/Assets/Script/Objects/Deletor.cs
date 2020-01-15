a9f8d5fab3a890f87b350652040661dc8cb0340c
kimgaejin <kimgaejin@naver.com>
2020-01-15 13:26:21 +0900

    삭제된 스크립트 복구
    . Cooler Creator 등등... 삭제된 스크립트 재생성

---
+++ gamejam/Assets/Script/Objects/Deletor.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deletor : MonoBehaviour
+{
+    private void Awake()
+    {
+        transform.tag = "danger";
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "platform") return;
+        if (collision.tag == "Player") return;
+
+        Destroy(collision.gameObject);
+    }
+}
