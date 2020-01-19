a2597d745a51183a9ee37aeb6fa438ad792c945b
kimgaejin <kimgaejin@naver.com>
2020-01-20 02:04:50 +0900

    튜토리얼용 WorldText 생성
    . 플레이어가 접근하면 스르륵 나오는 말풍선 생성

---
+++ gamejam/Assets/Script/CanvasObjects/WorldText.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldText : MonoBehaviour
+{
+    private Animator anim;
+    private RectTransform rectTransform;
+    private bool withPlayer;
+    private bool isOn;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        // 모든 자식들 set false
+        //SetActiveAllChild(false);
+        isOn = false;
+        withPlayer = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            withPlayer = true;
+            ////isOn = true;
+            anim.Play("ToTransparency020");
+            //SetActiveAllChild(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            anim.Play("ToTransparency100");
+            //withPlayer = false;
+           // SetActiveAllChild(false);
+            //StartCoroutine(SetActiveOffAllChild());
+        }
+    }
+
+    private void SetActiveAllChild(bool type)
+    {
+        foreach (Transform trans in transform)
+        {
+            trans.gameObject.SetActive(type);
+        }
+    }
+
+    IEnumerator SetActiveOffAllChild()
+    {
+        WaitForSeconds wait100 = new WaitForSeconds(1.0f);
+        while (true)
+        {
+            
+            yield return wait100;
+            isOn = false;
+        }
+    }
+}
