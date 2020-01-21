d80c341174a99b40a4bb81056d2c1b09e6831ff1
kimgaejin <kimgaejin@naver.com>
2020-01-22 02:25:59 +0900

    카메라 무빙 제한
    . 카메라가 특정 지역은 이동하지 못하도록 하는 기능
    . 시네머신 카메라가 이제 플레이어가 아니라 CameraAngle을 따라다님.
    . CameraAngle은 CameraAngleCollider에 의해 가로막힘.
    . 플레이어가 CameraJump 관련된 것과 부딪치면 CameraAngle이 플레이어에게 순간이동함.

---
+++ gamejam/Assets/Script/CameraCollision.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    private Vector3 beforePos;
+    private Transform tfCameraColl;
+    private CircleCollider2D playerCameraColl;
+    private bool isTouching;
+    private Transform tfPlayer;
+
+    private void Awake()
+    {
+        beforePos = transform.position;
+        tfPlayer = GameObject.Find("Player").transform;
+        tfCameraColl = tfPlayer.Find("CameraColl");
+        playerCameraColl = tfCameraColl.GetComponent<CircleCollider2D>();
+
+    }
+
+    private void Start()
+    {
+        transform.parent = transform.parent.parent;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = (tfPlayer.position - transform.position) * 5;
+        if (velocity.magnitude < 5) velocity = velocity.normalized * 5;
+        transform.GetComponent<Rigidbody2D>().velocity = velocity;
+        JumpToPlayer();
+    }
+
+    private bool JumpToPlayer()
+    {
+        if (Physics2D.IsTouchingLayers(playerCameraColl, -1))//LayerMask.NameToLayer("Camera")))
+        {
+            Collider2D[] colls = new Collider2D[20];
+            ContactFilter2D contactFilter = new ContactFilter2D();
+            contactFilter.SetLayerMask(1 << LayerMask.NameToLayer("Camera"));
+            playerCameraColl.OverlapCollider(contactFilter, colls);
+
+            foreach (Collider2D coll in colls)
+            {
+                try
+                {
+                    if (coll.name.Contains("Jump"))
+                    {
+                        Debug.Log("CAMERA JUMP!");
+                        transform.position = tfPlayer.position;
+                        break;
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+        }
+        return false;
+    }
+
+}
---
+++ gamejam/Assets/Script/Components/SetChildsTransparency.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetChildsTransparency : MonoBehaviour
+{
+    public float transparency = 0.0f;
+
+    private void Awake()
+    {
+        foreach (Transform trans in transform)
+        {
+            try
+            {
+                Color color = trans.GetComponent<SpriteRenderer>().color;
+                color = new Color(color.r, color.b, color.b, transparency);
+                trans.GetComponent<SpriteRenderer>().color = color;
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
